67fe1bf14ae7ef3fa428a754156d823809717d94
Hareni <hareniraj2002@gmail.com>
2025-07-11 09:36:27 +0530

    Add files via upload

---
+++ demodis/src/main/java/com/example/demo/DemodisApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodisApplication.class, args);
+	}
+
+}
